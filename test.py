4b85f109fd5e2ae83c712e8400acf0f4b45ef738
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-18 07:03:43 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 from transformers import pipeline
 import sent_preprocessing as sp
 
+text = "".join(sp.book[25:475])
+
+qa = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
+context = text
+question = query
 
 def ask_question(query):
-  text = "".join(sp.book[25:475])
-  
-  qa = pipeline("question-answering", model="deepset/minilm-uncased-squad2")
-  
-  context = text
-  question = query
-  
-  result = qa(question=question, context=context)
+  result = qa(question=question, context=context, batch_size=2, top_k=2, max_answer_len=30)
   answer = result["answer"]
   return answer
+
