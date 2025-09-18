aed384b7cd12668627f4c39f28b9452ef80e4ef1
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-18 06:46:41 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
 from transformers import pipeline
 import sent_preprocessing as sp
 
-text = "".join(sp.book[25:475])
 
-# Load a small QA model (lightweight for speed/memory)
-qa = pipeline("question-answering", model="deepset/minilm-uncased-squad2")
-
-# Provide context and question
-context = text
-# print(context)
-question = "what is influenza?"
-
-# Run QA
-result = qa(question=question, context=context)
-
-print(result["answer"])
+def ask_question(query):
+  text = "".join(sp.book[25:475])
+  
+  qa = pipeline("question-answering", model="deepset/minilm-uncased-squad2")
+  
+  context = text
+  question = query
+  
+  result = qa(question=question, context=context)
+  answer = result["answer"]
+  return answer
