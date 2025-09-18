c92de282d29485989c795598d61f77920c866c7c
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-18 07:04:30 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,14 @@ import sent_preprocessing as sp
 text = "".join(sp.book[25:475])
 
 qa = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
-context = text
-question = query
+
 
 def ask_question(query):
+  context = text
+  question = query
+  
   result = qa(question=question, context=context, batch_size=2, top_k=2, max_answer_len=30)
   answer = result["answer"]
   return answer
+
 
