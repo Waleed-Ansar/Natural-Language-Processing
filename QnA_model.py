20258b74917bbb40321402b62c6ed9335284df68
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-19 00:22:43 -0700

    Update QnA_model.py

--- QnA_model.py
+++ QnA_model.py
@@ -6,12 +6,13 @@ import pandas as pd
 pdf_df = pd.DataFrame(sp.sentences, columns=['text'])
 text = "".join(pdf_df)
 
-qa = pipeline("text2text-generation", model="google/flan-t5-large")
+qa = pipeline("text2text-generation", model="google/flan-t5-medium")
 
 def ask_question(query):
     prompt = f"question: {query}  context: {text}"
     result = qa(prompt, max_new_tokens=1024)
     return (f"{result[0]['generated_text']}\n")
+
 
 
 
