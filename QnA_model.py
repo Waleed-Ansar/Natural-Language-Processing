fce12690d22024dbd04c478d975c590d0c2388ae
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-19 06:42:08 -0700

    Update QnA_model.py

--- QnA_model.py
+++ QnA_model.py
@@ -3,24 +3,13 @@ import sent_preprocessing as sp
 import pandas as pd
 
 
-qa = pipeline("question-answering", model="distilbert-base-cased-distilled-squad", tokenizer="distilbert-base-cased-distilled-squad")
+qa = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
 
 def ask_question(query):
-    result = qa(
-        {
-            "question": query,
-            "context": sp.text
-        },
-        handle_impossible_answer=True,
-        topk=1,
-        max_answer_len=500,
-        doc_stride=128,
-        max_seq_len=512
-    )
-    
     result = qa({"question": query, "context": sp.text})
     answer = result["answer"]
     return answer
+
 
 
 
