66bf0365317ff85ef0bec9906e648ff5ab8e63e4
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-19 03:22:38 -0700

    Update QnA_model.py

--- QnA_model.py
+++ QnA_model.py
@@ -2,23 +2,14 @@ from transformers import pipeline
 import sent_preprocessing as sp
 import pandas as pd
 
-
 pdf_df = pd.DataFrame(sp.sentences, columns=['text'])
 text = "".join(pdf_df)
 
-qa = pipeline("text2text-generation", model="google/flan-t5-base")
+qa = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
 
 def ask_question(query):
     prompt = f"question: {query}  context: {text}"
-    result = qa(prompt, max_new_tokens=1024)
-    return (f"{result[0]['generated_text']}\n")
-
-
-
-
-
-
-
-
-
-
+    # result = qa(prompt, max_new_tokens=1024)
+    result = qa({"question": query, "context": text})
+    answer = result["answer"]
+    return answer
