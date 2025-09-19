41609c4afc6a25ceb53b77eb7d7cf0c1ab13e7a8
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-19 03:34:58 -0700

    Update QnA_model.py

--- QnA_model.py
+++ QnA_model.py
@@ -2,8 +2,6 @@ from transformers import pipeline
 import sent_preprocessing as sp
 import pandas as pd
 
-# pdf_df = pd.DataFrame(sp.sentences, columns=['text'])
-# text = "".join(pdf_df)
 
 qa = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
 
@@ -11,5 +9,3 @@ def ask_question(query):
     result = qa({"question": query, "context": sp.text})
     answer = result["answer"]
     return answer
-
-
