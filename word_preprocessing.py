cbb180e6d6260c774ea255807ff5e9f808e240a4
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-19 03:24:56 -0700

    Update word_preprocessing.py

--- word_preprocessing.py
+++ word_preprocessing.py
@@ -20,7 +20,7 @@ nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger_eng')
 
 # ==== Importing and Reading Data ====
-doc = fitz.open("John M. Barry - The Great Influenza - The story of the deadliest pandemic in history.pdf")
+doc = fitz.open("the-strange-case-of-doctor-jekyll-and-mr-hyde-robert-louis-stevenson.pdf")
 length = len(doc)
 
 book = []
@@ -98,6 +98,7 @@ def search_word(word):
 
 
 # python word_preprocessing.py
+
 
 
 
