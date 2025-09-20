798e75b0d3fb18f90e5d34469a758bfe3d842fc0
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-20 09:25:43 -0700

    Update word_preprocessing.py

--- word_preprocessing.py
+++ word_preprocessing.py
@@ -17,68 +17,69 @@ nltk.download('wordnet')
 nltk.download('punkt_tab')
 
 # ==== Importing and Reading Data ====
-doc = fitz.open("the-strange-case-of-doctor-jekyll-and-mr-hyde-robert-louis-stevenson.pdf")
-length = len(doc)
-
-book = []
-for i in range(length):
-    page = doc[i]
-    text = page.get_text()
-    book.append(text)
-
-df = pd.DataFrame(book)
-
-text = " ".join(book)
-
-tokens = word_tokenize(text)
-
-words = " ".join(tokens)
-
-# ==== Cleaning Data ====
-words = unicodedata.normalize("NFKC", words)
-words = re.findall(r"[A-Za-zÀ-ÿ]+", words)
-
 clean_words = []
+ner_words = []
 
-# ==== Lemmatizitation ====
-lemmatizer = WordNetLemmatizer()
+def w_main(pdf):
+    global book
+    book = pdf
 
-for word in words:
-    if word not in STOP_WORDS and not len(word) <= 3:
-        word = lemmatizer.lemmatize(word)
-        word = re.sub(r'\\n', "", word)
-        clean_words.append(word.lower())
+    global text
+    text = " ".join(book)
 
-# ==== POS ====
-pos = nltk.pos_tag(list(set(clean_words)))
+    global tokens
+    tokens = word_tokenize(text)
 
-def search_pos(word):
-    words = list(set(clean_words))
-    if word in words:
-        pos_list = []
-        pos_list.append(word)
-        query_pos = nltk.pos_tag(pos_list)
-        return query_pos
-
-# ==== NER ====
-text = "".join(book[40:106])
-
-ner = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english", aggregation_strategy="average")
-
-def all_ners():
-    entities = ner(text)
-
-    result = []
+    global words
+    words = " ".join(tokens)
     
-    for entity in entities:
-        entity_type = entity.get('entity_group', entity.get('entity', 'UNKNOWN'))
-        entity_name = entity['word']
-        result.append(f"{entity_name} --> {entity_type}")
- 
-    return (set(result))
+    # ==== Cleaning Data ====
+    words = unicodedata.normalize("NFKC", words)
+    words = re.findall(r"[A-Za-zÀ-ÿ]+", words)
 
+    global clean_words
+    clean_words = []
+    
+    # ==== Lemmatizitation ====
+    lemmatizer = WordNetLemmatizer()
+    
+    for word in words:
+        if word not in STOP_WORDS and not len(word) <= 3:
+            word = lemmatizer.lemmatize(word)
+            word = re.sub(r'\\n', "", word)
+            clean_words.append(word.lower())
+    
+    # ==== POS ====
+    global pos
+    pos = nltk.pos_tag(list(set(clean_words)))
+    
+    def search_pos(word):
+        words = list(set(clean_words))
+        if word in words:
+            pos_list = []
+            pos_list.append(word)
+            query_pos = nltk.pos_tag(pos_list)
+            return query_pos
+    
+    # ==== NER ====
+    text = "".join(book[40:106])
+    
+    ner = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english", aggregation_strategy="average")
+    result = []
+    def all_ners():
+        entities = ner(text)
+    
+        global results
+        
+        for entity in entities:
+            entity_type = entity.get('entity_group', entity.get('entity', 'UNKNOWN'))
+            entity_name = entity['word']
+            result.append(f"{entity_name} --> {entity_type}")
+     
+        return (set(result))
+ners = []
 def search_ner(word):
-    ners = []
+    global ners
     
     results = ner(word)
     for entity in results:
@@ -87,6 +88,7 @@ def search_ner(word):
         ners.append(f"{entity_name} --> {entity_type}")
     return ners
 # ==== Counting Data ====
+global counts
 counts = FreqDist(clean_words)
 
 def search_word(word):
@@ -97,6 +99,7 @@ def search_word(word):
 
 
 # python word_preprocessing.py
+
 
 
 
