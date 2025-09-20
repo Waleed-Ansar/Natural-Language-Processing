01e764b993c9c06a847c1b73a3c9088d4fede5a8
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-20 09:40:47 -0700

    Update sent_preprocessing.py

--- sent_preprocessing.py
+++ sent_preprocessing.py
@@ -10,7 +10,10 @@ nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger_eng')
 
 # ==== Importing and Reading Data ====
+
+sentences = []
 book = []
+
 def s_main(pdf):
     global book
     book = pdf
@@ -21,11 +24,13 @@ def s_main(pdf):
 
     tokens = sent_tokenize(text)
 
+    global sentences
     for token in tokens:
         sentences.append(token)
 
 
 # python sent_preprocessing.py
+
 
 
 
