fc3e676e0d72ae23d7ee57a5df74dd41a52683db
Waleed-Ansar <waleedansar570@outlook.com>
2025-09-25 05:28:02 -0700

    Update sent_preprocessing.py

--- sent_preprocessing.py
+++ sent_preprocessing.py
@@ -1,3 +1,4 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from nltk.tokenize import sent_tokenize
 from transformers import pipeline
 import pandas as pd
@@ -11,14 +12,18 @@ qa = pipeline("question-answering", model="distilbert-base-cased-distilled-squad
 
 # ==== Importing and Reading Data ====
 
+context = []
 sentences = []
 book = []
 tokens = []
 
-def s_main(pdf):
+def s_main(pdf, pdf_fitz):
     global book
     book = pdf
 
+    global context
+    context = pdf_fitz
+    
     global text
     text = book
     text = re.sub(r'\\n', "", text)
@@ -31,13 +36,21 @@ def s_main(pdf):
         sentences.append(token)
 
 
-def ask_question(query):
-    result = qa({"question": query, "context": book})
-    answer = result["answer"]
-    return answer
+tokenizer = AutoTokenizer.from_pretrained("t5-base")
+model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
+
+def ask_question(question: str, max_length=64):
+    df = pd.DataFrame(context, columns=['text'])
+    lines = df.text
+    print(lines)
+    input_text = f"question: {question}  context: keeping the book in consideration {lines}"
+    inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
+    out = model.generate(**inputs, max_length=max_length, num_beams=4)
+    return tokenizer.decode(out[0], skip_special_tokens=True)
 
 
 # python sent_preprocessing.py
+
 
 
 
