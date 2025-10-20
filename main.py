d474607d9e6afe0c411d28a2c31122a035050723
Waleed-Ansar <waleedansar570@outlook.com>
2025-10-20 04:56:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,3 @@ api = API()
 router.include_router(api.router)
 
 app.include_router(router, prefix='/book_ai')
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(
-        "main:app",
-        host="localhost",
-        port=8000,
-        reload=True
-    )
