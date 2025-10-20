4829bf3d13380a8ec2d91db1e862161f28252d02
Waleed-Ansar <waleedansar570@outlook.com>
2025-10-20 05:31:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, FastAPI
 from api import API
-
+import uvicorn
 
 app = FastAPI()
 router = APIRouter()
@@ -9,3 +9,11 @@ api = API()
 router.include_router(api.router)
 
 app.include_router(router, prefix='/book_ai')
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="localhost",
+        port=8000,
+        reload=True
+    )
