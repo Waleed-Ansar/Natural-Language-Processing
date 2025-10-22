8424a8c6f61a59f609e3aaca74efe4195deb9b1c
Waleed-Ansar <waleedansar570@outlook.com>
2025-10-22 03:56:48 -0700

    Delete connection.py

--- connection.py
+++
@@ -1,6 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient("mongodb+srv://Waleed-Ansar:fastAPI%40waleed570@fastapi-cluster.0yfuiu4.mongodb.net/")
-db = client["Book_Shelf"]
-collection_name = db['Books']
-api_keys = db['api_key']
